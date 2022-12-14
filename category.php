4120afb4b0f02f139df7ac5b8e6ca3769b550d02
yunusyerli1 <yunusyerli1@gmail.com>
2022-12-14 17:32:53 +0300

    category arranged

--- category.php
+++ category.php
@@ -10,7 +10,12 @@
             <!-- Blog Entries Column -->
             <div class="col-md-8">
                 <?php
-                    $query = "SELECT * from posts";
+
+                if(isset($_GET['category'])){
+                        
+                    $post_category_id  = $_GET['category'];
+                }
+                    $query = "SELECT * from posts WHERE post_category_id = $post_category_id ";
                     $select_all_posts_query = mysqli_query($connection, $query);
                     while($row = mysqli_fetch_assoc($select_all_posts_query)) {
                         $post_id = $row['post_id'];
