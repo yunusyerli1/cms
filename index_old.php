4e3677373186db34a1c30aac244dbd4b53ad7bc9
yunusyerli1 <yunusyerli1@gmail.com>
2022-12-19 14:27:35 +0300

    pagination done

--- index_old.php
+++ index_old.php
@@ -10,27 +10,7 @@
             <!-- Blog Entries Column -->
             <div class="col-md-8">
                 <?php
-                    $per_page = 1;
-                    if(isset($_GET['page'])) {
-                        $page = $_GET['page'];
-                    } else {
-                        $page = "";
-                    }
-
-                    if($page == "" || $page == 1) {
-                        $page_1 = 0;
-                    } else {
-                        $page_1 = ($page * $per_page) - $per_page;
-                    }
-
-                   // $post_query_count = "SELECT * FROM posts";
-                    $post_query_count = "SELECT * FROM posts WHERE post_status = 'published'";
-                    $find_count = mysqli_query($connection,$post_query_count);
-                    $count = mysqli_num_rows($find_count);
-
-                    $count = ceil($count / $per_page);
-
-                    $query = "SELECT * FROM posts LIMIT $page_1, $per_page";
+                    $query = "SELECT * FROM posts WHERE post_status = 'published'";
                     $select_all_posts_query = mysqli_query($connection, $query);
 
                     if(!$select_all_posts_query){
@@ -59,7 +39,6 @@
                             <small>Secondary Text</small>
                         </h1>
                           <!-- First Blog Post -->
-                          <h1><?php echo $count; ?></h1>
                             <h2>
                                 <a href="post.php?p_id=<?php echo $post_id ?>"><?php echo $post_title ?></a>
                             </h2>
@@ -97,19 +76,6 @@
         <!-- /.row -->
 
         <hr>
-
-        <ul class="pager">
-            <?php 
-            $number_list = array();
-            for($i =1; $i <= $count; $i++) {
-                if($i == $page) {
-                    echo "<li '><a class='active_link' href='index.php?page={$i}'>{$i}</a></li>";
-                }  else {
-                    echo "<li '><a href='index.php?page={$i}'>{$i}</a></li>";
-                } 
-            }
-            ?>
-</ul>
 
         
 <?php include "includes/footer.php" ?>
